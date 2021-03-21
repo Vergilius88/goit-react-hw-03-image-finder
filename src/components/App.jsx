fadee70d7d06245402680e984306c63266590ed4
Nikolaienko Ihor <nikolasha8876@gmail.com>
2021-03-21 16:29:27 +0200

    del concole log

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,7 +43,6 @@ export default class App extends Component {
           });
         })
         .catch((error) => {
-          console.dir(error);
           this.setState({ error: error.message, status: "reject" });
         });
     }
